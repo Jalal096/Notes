3d7d3d779122e033308efac989037a4b52a8adcd
Jalal <ansarijalalu.80@gmail.com>
2023-03-22 22:13:46 +0530

    Some changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,49 +1,31 @@
 // saveFile = () => {
-//     const name = document.getElementById('nam'),
-//           rollno = document.getElementById('roll'),
-//           category = document.getElementById('cat'),
-//           subject = document.getElementById('sub'),
-//           filename = document.getElementById('file'),
-//           file = document.getElementById('upload');
 
-//     let data = {
-//         'Name' : name.value,
-//         'Roll No' : rollno.value,
-//         'Cateory' : category.value,
-//         'Subject' : subject.value,
-//         'Filename' : filename.value,
-//         'File' : file.value
-//     }
-//     URL.createObjectURL(file);
-//     // file.save(file.value)
-
-//     console.log(data);
 // }
+// const name = document.getElementById('nam')
+
 
 saveFile = () => {
-    let fileElement = document.getElementById('upload')
-
-    // check if user had selected a file
-    if (fileElement.files.length === 0) {
-      alert('please choose a file')
-      return
-    }
-
-    let file = fileElement.files[0]
-
-    let formData = new FormData();
-    formData.set('file', file);
-
-    axios.post(`http://localhost:5500/uploads`, formData, {
-      onUploadProgress: progressEvent => {
-        const percentCompleted = Math.round(
-          (progressEvent.loaded * 100) / progressEvent.total
-        );
-        console.log(`upload process: ${percentCompleted}%`);
-      }
-    })
-      .then(res => {
-        console.log(res.data)
-        console.log(res.data.url)
-      })
+  const pname = document.getElementById('nam')
+  const rollno = document.getElementById('roll')
+  const category = document.getElementById('cat')
+  const subject = document.getElementById('sub')
+  const filename = document.getElementById('file')
+  const file = document.getElementById('upload')
+  const success = document.getElementById('success')
+  let messages = 0
+  if (pname.value === "" || rollno.value === "" || category.value === "" || subject.value === "" || filename.value === "" || file.value === "") {
+    messages = 1
   }
+  success.style.display = "block";
+  alert("File Uploaded")
+  
+  let data = {
+    'Name' : pname.value,
+    'Roll No' : rollno.value,
+    'Cateory' : category.value,
+    'Subject' : subject.value,
+    'Filename' : filename.value,
+    'File' : file.value
+} 
+  module.exports = data;
+}
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,27 @@
-var express = require('express');
-var router = express.Router();
-var formidable = require('formidable');
-var fs = require('fs');
+const express = require('express');
+const router = express.Router();
+const formidable = require('formidable');
+const fs = require('fs');
+const data = require('../public/js/script');
 
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+console.log(data)
 router.post('/fileupload', function (req, res) {
   var form = new formidable.IncomingForm();
   form.parse(req, function (err, fields, files) {
     var oldpath = files.filetoupload.filepath;
     var newpath = 'uploads/' + files.filetoupload.originalFilename;
     fs.rename(oldpath, newpath, function (err) {
-      if (err) throw err;
-      res.write('File uploaded and moved!');
+      if (err) {
+        res.write("Oops!! Something went wrong")
+      }
+      // res.write("Uploaded")
+      console.log(data)
+      res.redirect('upload.html')
       res.end();
     });
   });
